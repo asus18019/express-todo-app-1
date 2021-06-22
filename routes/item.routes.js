6f23d63e6e8b0f5f0d7d44f1d16caea64f3bb7be
asus18019 <72703431+asus18019@users.noreply.github.com>
2021-06-22 20:21:08 +0300

    Update and rename index.js to item.routes.js

---
+++ routes/item.routes.js
@@ -0,0 +1,42 @@
+const Router = require('express');
+const router = Router();
+const Item = require('../models/item');
+
+router.get('/', async (req, res) => {
+    try {
+        res.json(await Item.find())
+    } catch (e) {
+        return res.status(400).json(e);
+    }
+})
+
+router.post('/add', async (req, res) => {
+    try {
+        const { nickname, text, comment } = req.body;
+        const itemObj = await new Item({
+            nickname, date: getDate(), text, comment
+        })
+        await itemObj.save();
+        res.json(itemObj);
+    } catch (e) {
+        return res.status(400).json(e);
+    }
+});
+
+router.delete('/remove', async (req,res) => {
+    try {
+        const { id } = req.body;
+        if(id.trim() === '') throw new Error();
+        await Item.findByIdAndDelete(id);
+
+        res.json({'message': 'success'});
+    } catch (e) {
+        return res.status(400).json(e);
+    }
+})
+
+const getDate = () => {
+    return new Date().toString().substring(0, 24);
+}
+
+module.exports = router;
