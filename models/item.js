0e5cd934247f9d0a2d17a3dcf7ab8804efe392f3
asus18019 <72703431+asus18019@users.noreply.github.com>
2021-06-22 22:43:20 +0300

    fix

--- models/item.js
+++ models/item.js
@@ -3,8 +3,8 @@ const { Schema, model } = require('mongoose');
 const schema = new Schema({
     nickname: {type: String, required: true},
     date: {type: String, required: true},
-    text: {type: Number, required: true},
-    comment: {type: Number, required: true},
+    text: {type: String, required: true},
+    comment: {type: String, required: true},
 })
 
 module.exports = model('Item', schema);
